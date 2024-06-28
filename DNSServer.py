53ebba89a418054fe0e20c0634839cdf0689ffaa
hg2896 <hg2896@nyu.edu>
2024-06-27 22:13:47 -0700

    step1

--- DNSServer.py
+++ DNSServer.py
@@ -92,6 +92,23 @@ dns_records = {
             86400, #minimum
         ),
     },
+    'google.com.': {
+        dns.rdatatype.A: '192.168.1.103',
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+        dns.rdatatype.MX: [(10, 'mail.google.com.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.CNAME: 'www.google.com.',
+        dns.rdatatype.NS: 'ns.google.com.',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.SOA: (
+            'ns1.google.com.', #mname
+            'admin.google.com.', #rname
+            2023081401, #serial
+            3600, #refresh
+            1800, #retry
+            604800, #expire
+            86400, #minimum
+        ),
+    },
     'legitsite.com.': {
         dns.rdatatype.A: '192.168.1.104',
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
